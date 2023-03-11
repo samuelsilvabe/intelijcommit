224b19ec7245d0a6407a6966a1f80f8c1524b1f2
Samuel <samuel.silva.beserra@gmail.com>
2023-03-11 15:49:59 -0300

    Algumas modificações implementadas

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -4,6 +4,8 @@ import br.com.dio.model.Gato;
 public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
+        System.out.println(gato);
+        System.out.println("Estou apenas testando o Github");
         System.out.println("Estou apenas testando o Github");
     }
 }
